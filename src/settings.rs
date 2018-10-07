f97a65391cc01e09f913f69a38c48fc1c847c68d
mkamener <mischka.kamener@gmail.com>
2018-10-07 15:52:22 +1100

    Rescale pointer asset

--- src/settings.rs
+++ src/settings.rs
@@ -69,7 +69,7 @@ pub mod pickup_generator {
 }
 
 pub mod offscreen_pointer {
-    pub const SCALE: f64 = 0.3;
+    pub const SCALE: f64 = 1.0;
     pub const OFFSET: f64 = 60.0;
 
     pub const OBJ_SCALE: f64 = 0.6;
